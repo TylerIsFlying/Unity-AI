8ab785b49d1b8dd0fc17f6226e695d7d26e8a2e6
Tyler Peterson <7314tylerpeterson@gmail.com>
2020-03-02 09:37:19 -0800

    hello

--- Assets/Scripts/Behavior Tree/BehaviorManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BehaviorManager : MonoBehaviour
-{
-    public BehaviorScript behaviorStarter;
-    private IBehavior currentBehavior;
-}
--- Assets/Scripts/Behavior Tree/IBehavior.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public interface IBehavior
-{
-    bool Execute(GameObject o); // executes the code and return true or false based if it failed or success
-}
